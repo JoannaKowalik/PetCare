ecdbff205fad08fbc19297a9bc003a3a0b5857dd
odvorko <oleksandrdvorko1703@gmail.com>
2024-11-24 18:36:36 +0000

    footer and navbar connection and some js and styles changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,53 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const burgerMenu = document.querySelector(".navbar-toggler");
-    const navbarMenu = document.querySelector("#navbarNav");
-    const content = document.querySelector("main");
-    const footer = document.querySelector(".footer");
-    const navbarContainer = document.querySelector(".container-fluid");
-    const navbarBrand = document.querySelector(".navbar-brand");
-    const contactForm = document.querySelector("#contactForm");
-    const successMessageBlock = document.querySelector("#successMessageBlock");
-    const successMessage = document.querySelector("#successMessage");
+function initializeNavbar() {
+  const burgerMenu = document.querySelector(".navbar-toggler");
+  const navbarMenu = document.querySelector("#navbarNav");
+  const content = document.querySelector("main");
+  const footer = document.querySelector(".footer");
+  const navbarContainer = document.querySelector(".container-fluid");
+  const navbarBrand = document.querySelector(".navbar-brand");
 
-    burgerMenu.addEventListener("click", function() {
+  if (burgerMenu && navbarMenu) {
+    burgerMenu.addEventListener("click", function () {
       navbarMenu.classList.toggle("active");
-      
       content.classList.toggle("hidden");
       footer.classList.toggle("hidden");
       navbarContainer.classList.toggle("hidden");
       navbarBrand.classList.toggle("hidden");
     });
+  }
+}
 
-    contactForm.addEventListener("submit", function(e) {
-      e.preventDefault(); 
-    
-      contactForm.classList.add("hidden"); 
-      successMessage.classList.remove("hidden"); 
+fetch("navbar.html")
+  .then((response) => response.text())
+  .then((html) => {
+    document.getElementById("navbar").innerHTML = html;
+    initializeNavbar(); 
+  })
+  .catch((error) => {
+    console.error("Error loading the navbar:", error);
+  });
+
+fetch("footer.html")
+  .then((response) => response.text())
+  .then((html) => {
+    document.getElementById("footer").innerHTML = html;
+  })
+  .catch((error) => {
+    console.error("Error loading the footer:", error);
+  });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const contactForm = document.querySelector("#contactForm");
+  const successMessageBlock = document.querySelector("#successMessageBlock");
+  const successMessage = document.querySelector("#successMessage");
+
+  if (contactForm) {
+    contactForm.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      contactForm.classList.add("hidden");
+      successMessage.classList.remove("hidden");
       successMessageBlock.classList.remove("hidden");
     });
-  });
+  }
+});
